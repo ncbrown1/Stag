10740c0eefebf1a0cb44d10c4ea42c4ef7bed729
Nick Brown <nickbrown714@gmail.com>
2016-06-17 10:28:59 -0700

    simple example of server main

--- stag-server/webapp.py
+++ stag-server/webapp.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -6,7 +6,6 @@ app.config.from_object(__name__)
 @app.route('/')
 def hello():
     return "Hello, World!"
-
 
 if __name__ == '__main__':
     app.debug = True
